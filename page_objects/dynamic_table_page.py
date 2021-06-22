d993c55cf59c401cadcfeea9b66648aafc57d5d4
ratskevichdg <ratskevichdg@gmail.com>
2021-06-22 13:21:09 +0300

    fix dynamic table test

--- page_objects/dynamic_table_page.py
+++ page_objects/dynamic_table_page.py
@@ -3,14 +3,21 @@ from selenium.webdriver.common.by import By
 
 class DynamicTablePage(BasePage):
 
-    TABLE_COLUMN_HEADER = "/html/body/section/div/div/div[2]/div"
-    ANSWER = "/html/body/section/div/p[2]"
+    TABLE_COLUMN_HEADER = (By.XPATH, "/html/body/section/div/div/div[2]/div")
+    ANSWER = (By.XPATH, "/html/body/section/div/p[2]")
+    # TABLE_ROWS = (By.XPATH, "/html/body/section/div/div/div[3]")
+    # ROW = (By.XPATH, "//*[@role='row']")
+    CELL = (By.XPATH, "//*[@role='cell']")
     
     def find_cpu_value(self):
-        return self.driver.find_element(By.XPATH, *self.TABLE_COLUMN_HEADER)
+        column_headers = self.driver.find_element(*self.TABLE_COLUMN_HEADER).text.split()
+        cpu_value_index = column_headers.index("CPU")
+        cells = [cell.text for cell in self.driver.find_elements(*self.CELL)]
+        return cells[cells.index("Chrome") + cpu_value_index]
 
-        
+
+
     
-    # def find_answer(self):
-    #     self.driver.find_element(Byself.HIDDING_BUTTON)
+    def find_answer(self):
+        return self.driver.find_element(*self.ANSWER).text
         
--- tests/test_dynamic_table.py
+++ tests/test_dynamic_table.py
@@ -10,7 +10,7 @@ import unittest
 
 from page_objects.dynamic_table_page import DynamicTablePage
 
-class HiddenLayer(unittest.TestCase):
+class DynamicTable(unittest.TestCase):
 
     _multiprocess_can_split_ = True
 
@@ -19,15 +19,17 @@ class HiddenLayer(unittest.TestCase):
         self.driver.implicitly_wait(30)
         self.base_url = "http://uitestingplayground.com/dynamictable"
 
-    def find_cpu_data(self):
+    def test_find_cpu_data(self):
         driver = self.driver
         driver.get(self.base_url)
         dynamic_table_page = DynamicTablePage(driver)
-        dynamic_table_page.find_cpu_value()
+        self.assertIn(
+            dynamic_table_page.find_cpu_value(),
+            dynamic_table_page.find_answer()
+        ), "Values doesn't match"
         
     def tearDown(self):
         self.driver.quit()
-
 
 if __name__ == "__main__":
     unittest.main()
