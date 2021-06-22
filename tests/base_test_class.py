e057e2be165ce8ff5dc1b1c6492af6891ce7038a
ratskevichdg <ratskevichdg@gmail.com>
2021-06-23 00:39:11 +0300

    docker initialization

--- tests/base_test_class.py
+++ tests/base_test_class.py
@@ -2,13 +2,27 @@ import unittest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+# from selenium import webdriver
+        
+capabilities = {
+    "browserName": "chrome",
+    "browserVersion": "91.0",
+    "selenoid:options": {
+        "enableVNC": True,
+        "enableVideo": False
+    }
+}
 
 class BaseTestClass(unittest.TestCase):
 
     _multiprocess_can_split_ = True
 
     def setUp(self):
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        # self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver = webdriver.Remote(
+            command_executor="http://localhost:4444/wd/hub",
+            desired_capabilities=capabilities
+        )
         self.driver.implicitly_wait(16)
 
 
