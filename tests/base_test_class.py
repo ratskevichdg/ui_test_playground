555f5991e1b4739c5d0c843ba4325e4e9bb2a2c5
ratskevichdg <ratskevichdg@gmail.com>
2021-06-23 18:25:48 +0300

    chromedriver path

--- tests/base_test_class.py
+++ tests/base_test_class.py
@@ -33,7 +33,7 @@ class BaseTestClass(unittest.TestCase):
         self.options.add_argument("--headless")
         self.options.add_argument("--no-sandbox")
         self.driver = webdriver.Chrome(
-            executable_path="/usr/local/bin/chromedriver",
+            executable_path="/usr/bin/chromedriver",
             chrome_options=self.options
         ) 
         # self.driver = webdriver.Chrome(
