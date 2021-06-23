a111730d37b00ad23e7d4ff47c55916e4447dcd2
ratskevichdg <ratskevichdg@gmail.com>
2021-06-23 17:23:53 +0300

    add local driver and local browser

--- tests/base_test_class.py
+++ tests/base_test_class.py
@@ -2,27 +2,38 @@ import unittest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-# from selenium import webdriver
+from selenium import webdriver
         
-capabilities = {
-    "browserName": "chrome",
-    "browserVersion": "91.0",
-    "selenoid:options": {
-        "enableVNC": True,
-        "enableVideo": False
-    }
-}
+# capabilities = {
+#     "browserName": "chrome",
+#     "browserVersion": "91.0",
+#     "selenoid:options": {
+#         "enableVNC": True,
+#         "enableVideo": False
+#     }
+# }
+
+
+
 
 class BaseTestClass(unittest.TestCase):
 
     _multiprocess_can_split_ = True
+    DRIVER_LOCATION = "/usr/bin/chromedriver" 
+    BINARY_LOCATION = "/usr/bin/google-chrome"
 
     def setUp(self):
         # self.driver = webdriver.Chrome(ChromeDriverManager().install())
-        self.driver = webdriver.Remote(
-            command_executor="http://localhost:4444/wd/hub",
-            desired_capabilities=capabilities
-        )
+        # self.driver = webdriver.Remote(
+        #     command_executor="http://localhost:4444/wd/hub",
+        #     desired_capabilities=capabilities
+        # )
+        self.options = webdriver.ChromeOptions() 
+        self.options.binary_location = self.BINARY_LOCATION 
+        self.driver = webdriver.Chrome(
+            executable_path=self.DRIVER_LOCATION,
+            options=self.options
+        ) 
         self.driver.implicitly_wait(16)
 
 
